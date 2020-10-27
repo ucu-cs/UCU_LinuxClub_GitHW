01f669e3eab2b25c6c3af7893c185c6355306697
wardady <yavorskyi@ucu.edu.ua>
2020-10-27 11:28:59 +0200

    Added random quote choosing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from os import system, name
 import cowsay
 import json
-import random
+from os import system, name
+from random import choice
 
 CHARACTERS = ["beavis", "cheese", "daemon", "cow", "dragon", "ghostbusters", "kitty", "meow", "milk", "stegosaurus",
               "stimpy", "turkey", "turtle", "tux"]
@@ -13,8 +13,8 @@ def get_character():
     Returns:
         str - cowsay character
     """
-    character = random.choice(CHARACTERS)
-    return character
+    return choice(CHARACTERS)
+
 
 def get_quote():
     """
@@ -24,11 +24,13 @@ def get_quote():
     Returns:
         str - a quote
     """
-    return "This is quote from latest master branch! Random quotes coming soon!"
+    with open("quotes.json", "r") as file:
+        quotes = json.load(file)
+        quote_of_day = " ".join(choice(quotes))
+        return quote_of_day
 
 if __name__ == "__main__":
     character = get_character()
     print(f"{character.capitalize()} says:\n\n\n")
-
-    getattr(cowsay,character)(get_quote())
-
+    print("Quote of the day:\n\n\n")
+    getattr(cowsay, get_character())(get_quote())
